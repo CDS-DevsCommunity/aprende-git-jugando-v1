dd1d7fadd998398e44252b310ce8f45a8c1d6091
Bryan <b.a.h.007@live.com>
2025-03-29 15:33:56 -0400

    importando libreias faltantes

--- individual/bryana/src/main/java/com/bryana/proyecto/controller/ImageController.java
+++ individual/bryana/src/main/java/com/bryana/proyecto/controller/ImageController.java
@@ -7,8 +7,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.IOException;
 import java.nio.file.Files;
