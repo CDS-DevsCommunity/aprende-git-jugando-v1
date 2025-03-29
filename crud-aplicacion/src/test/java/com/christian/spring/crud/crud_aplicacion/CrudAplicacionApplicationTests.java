4b894cda99dfa4edfeeffb1751539d7ccbef13e0
ChristianMaldonadoCutile <christianmaldonadocutile@gmail.com>
2025-03-29 10:48:13 -0400

    Creando el Crud de usuario

---
+++ crud-aplicacion/src/test/java/com/christian/spring/crud/crud_aplicacion/CrudAplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christian.spring.crud.crud_aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
