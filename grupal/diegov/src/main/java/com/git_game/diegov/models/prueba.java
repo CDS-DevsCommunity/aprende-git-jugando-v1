716b0eaf1f46526ffec52345d6f40430b17db24e
DAVV422 <diegoalberto42216@gmail.com>
2025-03-29 21:11:38 -0400

    feat: añadir atributos al archivo prueba del proyecto grupal

--- grupal/diegov/src/main/java/com/git_game/diegov/models/prueba.java
+++ grupal/diegov/src/main/java/com/git_game/diegov/models/prueba.java
@@ -1,3 +1,10 @@
-public class prueba {
+package com.git_game.diegov;
 
+import jakarta.persistence.*;
+
+@Entity
+public class prueba {
+    private int user_id;
+    private string name_user;
+    private boolean user_verificado;
 }
