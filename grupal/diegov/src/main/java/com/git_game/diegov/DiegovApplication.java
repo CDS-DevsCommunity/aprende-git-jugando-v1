aa0781c3edcbb739d25dbeeb1fa1a74074f7091a
DAVV422 <diegoalberto42216@gmail.com>
2025-03-29 20:41:39 -0400

    feat: añadir proyecto grupal

---
+++ grupal/diegov/src/main/java/com/git_game/diegov/DiegovApplication.java
@@ -0,0 +1,13 @@
+package com.git_game.diegov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiegovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiegovApplication.class, args);
+	}
+
+}
