7170e344c2ae2c969b6acaecb102ffe452b4c1f8
ChristianMaldonadoCutile <christianmaldonadocutile@gmail.com>
2025-03-29 21:10:59 -0400

    Cambios

--- grupal/diegov/src/main/java/com/git_game/diegov/models/prueba.java
+++ grupal/diegov/src/main/java/com/git_game/diegov/models/prueba.java
@@ -3,4 +3,8 @@ public class prueba {
     private int celular;
     private string nombre; 
     private String email;
+
+    private String apellido;
+    private Integer edad;
+    private String direccion;
 }
