b0a4840e6de2e88198228735beef9a03d0eb0f18
DAVV422 <diegoalberto42216@gmail.com>
2025-03-29 20:27:21 -0400

    fix: eliminacion de los readme

---
+++ crud_usuario/src/main/java/com/christian/spring/crud_usuario/servicio/EstudianteServicio.java
@@ -0,0 +1,9 @@
+package com.christian.spring.crud_usuario.servicio;
+
+import java.util.List;
+
+import com.christian.spring.crud_usuario.entidad.Usuario;
+
+public interface EstudianteServicio {
+    public List<Usuario> listarTodosLosUsuarios();
+}
